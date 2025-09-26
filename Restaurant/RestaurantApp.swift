762d3b849778a2650718af96195bb3b0c00f75b2
Greg Swan <gmswan99@gmail.com>
2025-09-26 18:44:06 -0400

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Greg Swan on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
