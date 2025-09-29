ff829f6a0edbac34bfe15e6ea5e4628a6bb7dd5e
Greg Swan <gmswan99@gmail.com>
2025-09-29 13:24:52 -0400

    completed galleryview

--- Restaurant/GalleryView.swift
+++ Restaurant/GalleryView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    @State var photoData = [String]()
+    var dataService = DataService()
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack (alignment: .leading) {
+            Text("Gallery")
+                .font(.largeTitle)
+                .bold()
+            GeometryReader { proxy in
+               
+                ScrollView (showsIndicators: false) {
+                    LazyVGrid(columns:
+                                [GridItem(spacing:10),
+                                 GridItem(spacing:10),
+                                 GridItem(spacing:10)],
+                    spacing: 10) {
+                        ForEach(photoData, id: \.self) { p in
+                            Image(p)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(maxWidth: (proxy.size.width-20)/3)
+                                .clipped()
+                        }
+                    }
+                }
+                
+            }
+            .padding(.horizontal)
+            
+            
+        }.onAppear {
+            photoData = dataService.getPhotos()
+        }
     }
 }
 
